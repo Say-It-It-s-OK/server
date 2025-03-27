f644291357d054a3d91fb04abb6bde0362571599
Rubytubi <seongmin001015@naver.com>
2025-03-27 16:44:57 +0900

    [SERV] #3: 프로젝트 세팅
    - swagger 설정
    - 프로젝트 구조
    - README.md 파일 내용 수정

---
+++ swagger/swagger.js
@@ -0,0 +1,22 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Say it, it’s OK - Voice Kiosk API",
+      version: "1.0.0",
+      description: "음성 키오스크 백엔드 API 명세서",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "개발 서버",
+      },
+    ],
+  },
+  apis: ["../routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+module.exports = swaggerSpec;
