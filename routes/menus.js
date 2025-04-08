da91ebd4b239c026434e837e093c3758181c6fc3
Rubytubi <seongmin001015@naver.com>
2025-04-08 21:37:46 +0900

    /routes/menus.js swagger 주석 수정

--- routes/menus.js
+++ routes/menus.js
@@ -6,11 +6,11 @@ const Menu = require("../models/menu");
  * @swagger
  * /menus:
  *   get:
- *     summary: 전체 메뉴 조회
- *     description: 등록된 모든 메뉴 데이터를 반환합니다
+ *     summary: 모든 메뉴 조회
+ *     description: 등록된 모든 메뉴 정보를 반환합니다
  *     responses:
  *       200:
- *         description: 메뉴 리스트 응답
+ *         description: 메뉴 리스트 반환
  *         content:
  *           application/json:
  *             schema:
@@ -18,14 +18,34 @@ const Menu = require("../models/menu");
  *               items:
  *                 type: object
  *                 properties:
+ *                   id:
+ *                     type: string
  *                   name:
  *                     type: string
  *                   type:
  *                     type: string
+ *                     enum: [커피, 디카페인, 음료, 티, 디저트]
  *                   price:
  *                     type: number
  *                   options:
  *                     type: object
+ *                     properties:
+ *                       온도:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                       샷:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                       샷 추가:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                       크기:
+ *                         type: array
+ *                         items:
+ *                           type: string
  *                   ingredient:
  *                     type: array
  *                     items:
