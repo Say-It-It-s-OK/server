b52ae838f540e5d82d6a2a47a795efe3967b422e
Rubytubi <seongmin001015@naver.com>
2025-04-08 21:23:01 +0900

    [SERV] #7: menu에 swagger 주석 추가

--- routes/menus.js
+++ routes/menus.js
@@ -2,6 +2,36 @@ const express = require("express");
 const router = express.Router();
 const Menu = require("../models/menu");
 
+/**
+ * @swagger
+ * /menus:
+ *   get:
+ *     summary: 전체 메뉴 조회
+ *     description: 등록된 모든 메뉴 데이터를 반환합니다
+ *     responses:
+ *       200:
+ *         description: 메뉴 리스트 응답
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   type:
+ *                     type: string
+ *                   price:
+ *                     type: number
+ *                   options:
+ *                     type: object
+ *                   ingredient:
+ *                     type: array
+ *                     items:
+ *                       type: string
+ */
+
 router.get("/", async (req, res) => {
   try {
     const menus = await Menu.find();
