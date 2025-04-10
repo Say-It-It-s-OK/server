baf43664cc174d59d3f915401d2b5f8467c10b1c
Rubytubi <seongmin001015@naver.com>
2025-04-10 21:08:00 +0900

    [SERV] #14: 불필요한 주석 제거 및 코드 개선

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger/swagger");
 const menusRouter = require("./routes/menus");
-
 require("./db");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/menus", require("./routes/menus"));
+
+app.use("/menus", menusRouter);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 if (process.env.NODE_ENV !== "test") {
