4dfcec52c384f08a1b5276feeb9b31d0c3556285
Rubytubi <seongmin001015@naver.com>
2025-04-10 21:04:40 +0900

    [SERV] #14: 클라이언트 캐시 관리 로직 & DB 데이터 동기화
    - 초기 데이터 제공
    - 데이터 변경 여부 확인

--- app.js
+++ app.js
@@ -6,23 +6,22 @@ const menusRouter = require("./routes/menus");
 require("./db");
 
 const app = express();
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use("/menus", require("./routes/menus"));
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.listen(port, function () {
-  console.log(`Connect to ${port} port!`);
-  console.log(`Swagger 문서: http://localhost:${port}/api-docs`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, function () {
+    console.log(`Connect to ${port} port!`);
+    console.log(`Swagger 문서: http://localhost:${port}/api-docs`);
+  });
+}
 
-app.get("/", function (req, res) {
-  res.send("말하면 OK 서버 실행 중");
-});
+// app.get("/", function (req, res) {
+//   res.send("말하면 OK 서버 실행 중");
+// });
 
 // sample data 삽입 테스트 후 삭제 예정 -> 일단 지금은 주석처리
 // const devRoutes = require("./routes/.dev");
