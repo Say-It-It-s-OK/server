23ba79d8b37274946c601e69a89d2843a20301f7
sooin <2001peace88@gmail.com>
2025-05-23 00:21:11 +0900

    [SERV] #31: 복합 주문 처리 맞춰 구조 변경경

--- controllers/nlpController.js
+++ controllers/nlpController.js
@@ -92,12 +92,7 @@ exports.handleNLPRequest = async (req, res) => {
       }
 
       if (action === "confirm") {
-        console.log("[CART] FETCH:", {
-          sessionId,
-          cart: cache.getCart(sessionId),
-        });
         return queryController.handleConfirm(req, res, payload);
-        
       }
 
       if (action === "help") {
